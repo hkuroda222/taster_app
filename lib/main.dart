1713ce0020ebb0c68e36c9b2772a4ab7fb5dbdb1
HirokiKuroda <=>
2021-12-13 23:21:45 +0900

    nullエラー修正

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() async {
-  await Firebase.initializeApp();
+void main() {
   runApp(const MyApp());
 }
 
