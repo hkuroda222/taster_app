f03afde04892e6a57f2c846ce94f38851dc0b49a
HirokiKuroda <=>
2021-12-13 21:08:29 +0900

    firebaseの設定を追加

--- android/app/src/main/kotlin/com/example/taster/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/taster/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.taster
+package com.taster.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
