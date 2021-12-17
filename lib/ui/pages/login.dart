4ac04e639ebe30d194db32616b97fa0fea0cd6b4
HirokiKuroda <=>
2021-12-17 22:51:11 +0900

    新規アカウント作成処理作成

--- lib/ui/pages/login.dart
+++ lib/ui/pages/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'package:firebase_auth/firebase_auth.dart';
+
+import './home.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -14,8 +15,6 @@ class _LoginPage extends State<Login> {
 
   // Firebase Authentication用インスタンス
   final FirebaseAuth auth = FirebaseAuth.instance;
-  // AuthResult result;
-  // FirebaseUser user;
 
   @override
   Widget build(BuildContext context) {
@@ -65,29 +64,23 @@ class _LoginPage extends State<Login> {
                 style: ElevatedButton.styleFrom(
                   primary: Colors.grey, //ボタンの背景色
                 ),
-
-                // 遷移処理
                 onPressed: () async {
-                  // try {
-                  // メール/パスワードでユーザー登録
-                  //  result = await auth.signInWithEmailAndPassword(
-                  //     email: email,
-                  //     password: password,
-                  //   );
-                  // ログイン成功
-                  // ログインユーザーのIDを取得
-                  // user = result.user;
-                  // Navigator.push(
-                  //     context,
-                  //     MaterialPageRoute(
-                  //       builder: (context) => Home(user_id: user.uid),
-                  //     ));
-                  // } catch (e) {
+                  try {
+                  // メール・パスワードでユーザー登録
+                    UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                  //遷移処理(現状はホームへ遷移)
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Home(),
+                      ));
+                  } catch (e) {
                   // ログインに失敗した場合
                   // setState(() {
                   //   infoText = auth_error.login_error_msg(e.code);
                   // });
-                  // }
+                  }
                 }),
           ),
         ),
